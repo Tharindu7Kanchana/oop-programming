a152c0f9a7ddadebfd2fe0466dbdd67f151105d6
Tharindu7Kanchana <48875349+Tharindu7Kanchana@users.noreply.github.com>
2021-08-20 20:06:17 +0530

    final

--- oop-programming/src/com/week4/q1/teacher.java
+++ oop-programming/src/com/week4/q1/teacher.java
@@ -3,30 +3,45 @@ package com.week4.q1;
 
 public class teacher extends person{
 	private int numCourses = 0;
-	private String[] courses = {};
+	private String[] courses = new String[10];
 	
 	teacher(String name, String address) {
 		super(name, address);	
 	}
 	public boolean addCourse(String course) {
 		for(String k:this.courses) {
-			if(k==course)
+			if(k==null)
+				break;
+			if(k.equals(course))
 				return false;
 		}
+		
 		this.courses[numCourses] = course;
-		numCourses+=1;
+		this.numCourses+=1;
 		return true;
 	}
 	public boolean removeCourse(String course) {
-		for(String k:this.courses) {
-			if(k==course)
+		int i=0;
+		for(String k:this.getCourses()) {
+			if(k==null)
+				break;
+			if(k.equals(course))
+			{
+				this.getCourses()[i] = null;
+				this.numCourses-=1;
 				return true;
+			}
+			i++;	
 		}
-		this.courses[courses.length] = null;
-		this.numCourses-=1;
 		return false;
 	}
 	public String toString() {
 		return "Teacher: "+name+'('+address+')';
+	}
+	public String[] getCourses() {
+		return courses;
+	}
+	public void setCourses(String[] courses) {
+		this.courses = courses;
 	}
 }
--- oop-programming/src/com/week4/q1/testPerson.java
+++ oop-programming/src/com/week4/q1/testPerson.java
@@ -4,17 +4,40 @@ import java.util.Scanner;
 
 public class testPerson {
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Enter name and address");
 		String name=sc.nextLine();
 		String address=sc.nextLine();
+		
 		student st = new student(name,address);
+		teacher te = new teacher(name,address);
+		
 		System.out.println("Enter courses with relevant grade");
 		for(int i=0;i<3;i++) {
-			st.addCourseGrade(sc.next(), sc.nextInt());
+			String course=sc.next();
+			int grade=sc.nextInt();
+			st.addCourseGrade(course, grade);
+			te.addCourse(course);
 		}
+		sc.close();
+		
 		System.out.println(st.toString());
 		st.printGrades();
 		System.out.println('\n'+st.getAverageGrade());
+		
+		
+		for(String k:te.getCourses()) {
+			if(k!=null)
+				System.out.println(k);
+		}
+		System.out.println(te.toString());
+		System.out.println(te.removeCourse("eng"));
+		for(String k:te.getCourses()) {
+			if(k!=null)
+				System.out.println(k);
+		}
+		
 	}
 }
